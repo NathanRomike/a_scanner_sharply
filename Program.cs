650fe28aab6a3322c700105479921532aff73c53
nathanromike <nathanromike@gmail.com>
2018-05-13 10:41:17 -0700

    Add exception handle around process

--- Program.cs
+++ Program.cs
@@ -5,10 +5,8 @@ namespace AgentLogProcessor
 {
   internal static class Program
   {
-    private const string Bash = "/bin/bash";
     private const string PackageName = "com.newrelic.android";
     private const string OutputFileName = "runtime.log";
-    
     private static Process _process;
     
     public static void Main(string[] args)
@@ -18,20 +16,28 @@ namespace AgentLogProcessor
 
     private static void ConnectLogcatBuffer()
     {
-      _process = new Process
+      try
       {
-        StartInfo = GetStartInfo()
-      };
-      _process.Start();
-      _process.WaitForExit();
+        _process = new Process
+        {
+          StartInfo = GetStartInfo()
+        };
+        _process.Start();
+        _process.WaitForExit();
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message + e.HelpLink);
+        Console.Write(e.StackTrace);
+      }
     }
 
     private static ProcessStartInfo GetStartInfo()
     {
       return new ProcessStartInfo
         {
-          FileName = Bash,
-          Arguments = $"-c \"adb logcat -d {PackageName}:* *:S > {OutputFileName}\""
+          FileName = "/bin/bash",
+          Arguments = $"-c \"adb logcat -d {PackageName}:* *:S > {OutputFileName} && adb logcat -c\""
         };
     }
   }
